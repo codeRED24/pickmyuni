ccb1e4684686e774457bf158d4c55b5f2ac9bf5e
Manujdixit <manujdixit04@gmail.com>
2025-06-20 22:18:07 +0530

    update

---
+++ backend/prisma/migrations/20250620163845_update/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Made the column `slug` on table `Articles` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "Articles" ALTER COLUMN "slug" SET NOT NULL,
+ALTER COLUMN "slug" DROP DEFAULT;
